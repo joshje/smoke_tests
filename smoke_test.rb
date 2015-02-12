4bb4c0ccb9dcfc44eba7e236fbc781706011d6e3
Steven Wilkin <stevenwilkin@gmail.com>
2015-02-12 16:49:37 +0000

    Add first cut of smoke test

---
+++ smoke_test.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+
+require 'faraday'
+
+domain = 'https://www.pensionwise.gov.uk'
+pages = %w(
+  /
+  /6-steps-you-need-to-take
+  /benefits
+  /complaints
+  /divorce
+  /living-abroad
+  /making-money-last
+  /pension-complaints
+  /pension-pot-options
+  /pension-pot-value
+  /pension-types
+  /scams
+  /shop-around
+  /tax
+  /work-out-income
+)
+
+conn = Faraday.new(domain)
+
+for page in pages do
+  puts page
+  response = conn.get(page)
+
+  unless response.status == 200
+    raise "Status code was #{response.status}"
+  end
+
+  unless DateTime.parse(response.headers['expires']).to_time > Time.now
+    raise "Expires at #{response.headers['expires']}"
+  end
+end
