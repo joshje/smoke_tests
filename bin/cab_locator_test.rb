6454725fb7db80e8d1a301513b310cb9581412cd
Steven Wilkin <stevenwilkin@gmail.com>
2015-07-06 13:31:15 +0100

    Test the cab locator more thoroughly

---
+++ bin/cab_locator_test.rb
@@ -0,0 +1,53 @@
+#!/usr/bin/env ruby
+
+require 'bundler/setup'
+require 'date'
+require 'mechanize'
+
+domain = ARGV[0]
+
+puts ">> Checking #{domain} at #{Time.now}"
+
+mech = Mechanize.new
+
+if ENV['AUTH_USERNAME'] && ENV['AUTH_PASSWORD']
+  mech.auth('username', 'password')
+end
+
+page = mech.get("#{domain}/locations")
+
+if page.body =~ /Find a face-to-face appointment location/
+  puts '> Renders location finder'
+else
+  raise 'Should render location finder'
+end
+
+form = page.forms.first
+form['postcode'] = 'LONDON'
+page = form.submit(form.buttons.first)
+
+if page.body =~ /is not a valid postcode/
+  puts '> Renders invalid postcode message'
+else
+  raise 'Should render invalid postcode message'
+end
+
+form = page.forms.first
+form['postcode'] = 'SW1A 2HQ'
+page = form.submit(form.buttons.first)
+
+if page.body =~ /Face-to-face locations near/
+  puts '> Renders locations'
+else
+  raise 'Should render locations'
+end
+
+page = page.link_with(dom_class: 't-name').click
+
+if page.body =~ /To book an appointment at/
+  puts '> Renders location'
+else
+  raise 'Should render location'
+end
+
+puts '>> OK'
