7329b2eff68a0a9d6c8cb5c82ac79b54367038c1
Steven Wilkin <stevenwilkin@gmail.com>
2015-08-11 14:44:18 +0100

    Test the Switchboard app

--- domain/staging.rb
+++ domain/staging.rb
@@ -4,4 +4,5 @@ class Staging < Environment
   run "./bin/pension_guidance_test.rb #{ENV['PENSION_GUIDANCE_STAGING_DOMAIN']}"
   run "./bin/cab_locator_test.rb #{ENV['PENSION_GUIDANCE_STAGING_DOMAIN']}"
   run "./bin/output_document_test.rb #{ENV['OUTPUT_DOCUMENT_STAGING_DOMAIN']}"
+  run "./bin/switchboard_test.rb #{ENV['SWITCHBOARD_STAGING_DOMAIN']}"
 end
