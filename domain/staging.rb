222f660d32a058d53f40bdeda9ab784ff593bbc2
Ben Lovell <benjamin.lovell@gmail.com>
2016-08-14 18:36:16 +0100

    Planner backend test
    Just tests that signon is hooked up, the app loads and booking requests
    are viewed, thus the locations API is also accessible.
    
    I added configuration for the staging environment for completion-sake
    but it's likely we'll remove the staging configuration across all
    checks soon, since we get lots of false-negatives due to the time it
    takes to spin up heroku dynos in the staging environments.

--- domain/staging.rb
+++ domain/staging.rb
@@ -5,4 +5,5 @@ class Staging < Environment
   run "./bin/cab_locator_test.rb #{ENV['PENSION_GUIDANCE_STAGING_DOMAIN']}"
   run "./bin/output_document_test.rb #{ENV['OUTPUT_DOCUMENT_STAGING_DOMAIN']}"
   run "./bin/twilio_redirect_test.rb #{ENV['TWILIO_REDIRECT_STAGING_DOMAIN']}"
+  run "./bin/planner_backend_test.rb #{ENV['PLANNER_STAGING_DOMAIN']}"
 end
