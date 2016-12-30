6fd943e43214ed166b8274fe8509837547ac572d
Ben Lovell <benjamin.lovell@gmail.com>
2016-12-30 14:41:36 +0000

    Add checks for TAP
    This simply logs the smoke test user in, redirects to the allocations
    view (since the smoke test user has the `resource_manager` permission)
    and checks that Waheeda appears as an available resource.

--- domain/staging.rb
+++ domain/staging.rb
@@ -6,4 +6,5 @@ class Staging < Environment
   run "./bin/output_document_test.rb #{ENV['OUTPUT_DOCUMENT_STAGING_DOMAIN']}"
   run "./bin/twilio_redirect_test.rb #{ENV['TWILIO_REDIRECT_STAGING_DOMAIN']}"
   run "./bin/planner_backend_test.rb #{ENV['PLANNER_STAGING_DOMAIN']}"
+  run "./bin/tap_backend_test.rb #{ENV['TAP_STAGING_DOMAIN']}"
 end
